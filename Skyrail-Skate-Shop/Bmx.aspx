d3bdc522cce05a127802ab293a0ced389d3f7c52
muchem <melissauchem@gmail.com>
2020-01-23 21:20:06 -0600

    render bmx bike listings with datalist control.

--- Skyrail-Skate-Shop/Bmx.aspx
+++ Skyrail-Skate-Shop/Bmx.aspx
@@ -3,4 +3,31 @@
     <link rel ="stylesheet" href ="Content/Bmx.css" />
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div class ="jumbotron">
+           
+        </div>
+    <div class ="container">
+        <div class ="row">
+            <div class ="col-md-2">
+     
+                </div>
+            <div class="col-md-10">
+                  <asp:DataList ID="DataViewG"
+         
+                      RepeatDirection="Horizontal"
+                       RepeatColumns="3"
+                 runat="server">
+         <ItemTemplate>
+            
+                  <div class="img-box">
+                           <img src ="<%#Eval("pImage") %>" />
+                                 </div>
+                     <h6 class ="text-center"style ="margin:0;"><small><b><%#Eval("Pname") %></b></small></h6>
+                     <p class ="text-center">$<%#Eval("Pprice") %></p>
+             
+              </ItemTemplate>
+         </asp:DataList>
+            </div>
+        </div>
+    </div>
 </asp:Content>
--- Skyrail-Skate-Shop/Bmx.aspx.cs
+++ Skyrail-Skate-Shop/Bmx.aspx.cs
@@ -4,14 +4,34 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace Skyrail_Skate_Shop
 {
     public partial class Bmx : System.Web.UI.Page
     {
+        SqlConnection Connection = new SqlConnection(@"Data Source = localhost;Database = Skyrail;Integrated Security = SSPI");
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            if (!IsPostBack)
+            {
+                RenderBmx();
+            }
+        }
+        public void RenderBmx()
+        {
+            if(Connection.State == ConnectionState.Closed)
+            {
+                Connection.Open();
+                SqlDataAdapter sqladpt = new SqlDataAdapter("SelectBmxBikes", Connection);
+                sqladpt.SelectCommand.CommandType = CommandType.StoredProcedure;
+                DataTable dataTb = new DataTable();
+                sqladpt.Fill(dataTb);
+                Connection.Close();
+                DataViewG.DataSource = dataTb;
+                DataViewG.DataBind();
+            }
         }
     }
 }
